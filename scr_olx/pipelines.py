a79d04ddb77bc5a95aae00a5d89dfba7e5fdd7fe
Ku-Al <kubov.aleksandr@gmail.com>
2015-10-21 22:31:04 +0300

    add new file spider_f.py

--- scr_olx/pipelines.py
+++ scr_olx/pipelines.py
@@ -10,22 +10,41 @@ from scrapy import signals
 import json
 import codecs
 
-
 class ScrOlxPipeline(object):
     def __init__(self):
-#        self.file = open('ScrOlx.json', 'wb')
-         self.file = codecs.open('data_utf8.json', 'w', encoding='utf-8')
+         self.file = codecs.open('olx_home.html', 'w', encoding='utf-8')
+         self.file.write(u'<html lang="ru-RU">\n')
+         self.file.write(u'<head>\n')
+         self.file.write(u'<title>Grab наOlx;)</title>\n') #.encode('utf-8'))
+         self.file.write(u'<meta charset="UTF-8" />\n')
+         self.file.write(u'</head>\n')
+         self.file.write(u'<body>\n')
+
+
 
     def process_item(self, item, spider):
-       
+#      strFile = [] u'<a href="{}">{}</a>' 
 #           for aUrl in item:
 #              line = json.dumps(aUrl['url']) + "\n"
 #              line = json.dumps(aUrl['title'], ensure_ascii=False) + "\n"
-        line = json.dumps(dict(item), ensure_ascii=False, indent=4) + "\n"
+        i = 0
+        while i < len(item['url']):
+            strFile = u'<a href="'+item['url'][i]+u'">'+item['title'][i]+u'</a> Date:'+item['data'][i]+' Cost:'+item['cost'][i]+u'<br>\n'
+            i += 1
+            self.file.write(strFile) #.encode('utf-8'))
+
+#            self.file.write(item)
+#            self.file.write(title)
+#            self.file.write(str(ii['data'])) 
+#            self.file.write(str(ii['cost']))
+#            print('!--> ', ii)
+#       line = json.dumps(dict(item), ensure_ascii=False, indent=4) + "\n"
 #OrderedDict(item)
-        self.file.write(line)
+#       self.file.write(line)
         return item
     
     def spider_closed(self, spider):
-        self.file.close()
+        self.file.write(u'</body>\n')
+        self.file.write(u'</html>\n')
+        self.file.close() 
 
---
+++ scr_olx/spiders/spider_h.py
@@ -0,0 +1,54 @@
+import re
+import json
+from urlparse import urlparse
+import urllib
+
+from scrapy.selector import Selector
+try:
+    from scrapy.spiders import Spider
+except:
+    from scrapy.spiders import BaseSpider as Spider
+from scrapy.utils.response import get_base_url
+from scrapy.spiders import CrawlSpider, Rule
+#from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor as sle
+
+from scr_olx.items import *
+#from misc.log import *
+#from misc.spider import CommonSpider
+ 
+
+#from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+#from src_olx.items import ScrOlxItem 
+
+#from scrapy.loader.processor import TakeFirst
+#from scrapy.loader import XPathItemLoader
+#from scrapy.selector import HtmlXPathSelector
+#from orphanage.items import OrphanageItem
+
+
+class OlxSpider(CrawlSpider):
+    name = "scr_olx"
+    allowed_domains = ['odessa.od.olx.ua']
+    start_urls = ['http://odessa.od.olx.ua/nedvizhimost/arenda-domov/dolgosrochnaya-arenda-domov/']
+
+    rules = [
+        # The first rule to follow a link from this page - follow=True
+        #Rule(LinkExtractor(allow=('?page=', )), follow=True),
+        # Second rule for the pages on which we extract information
+#        Rule(LinkExtractor(allow='[0-5]',), callback='parse_item')
+        Rule(LinkExtractor(allow='\?page=[1-5]',), callback='parse_item')
+    ]
+
+    def parse_item(self, response):
+
+#        items = []   
+        item = ScrOlxItem()
+
+        item['url'] = response.xpath('//h3[@class="x-large lheight20 margintop5"]/a/@href').extract()
+        item['title'] = response.xpath('//h3[@class="x-large lheight20 margintop5"]/a/strong/text()').extract()
+        item['data'] = response.xpath('//p[@class="color-9 lheight16 marginbott5 x-normal"]/text()').extract()
+        item['cost'] = response.xpath('//p[@class="price"]/strong/text()').extract()
+#        items.append(item) 
+#        print items    
+        return item
